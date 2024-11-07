8f1ccb54ea20d2b76354e107d1863fa11faf83b1
Vartholomaios T <v.tolos@pragma-iot.com>
2024-11-07 14:32:59 +0200

    added port

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 morgan.token("body", (req) => JSON.stringify(req.body));
 
@@ -86,7 +88,7 @@ app.get("/info", (request, response) => {
   `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
