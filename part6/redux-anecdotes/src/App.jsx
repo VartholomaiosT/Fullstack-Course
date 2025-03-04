33691228195fae2a2744e1813beeb5b040df6f3b
Vartholomaios T <v.tolos@pragma-iot.com>
2025-03-04 13:20:23 +0200

    exercises 6.14 - 6.15

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -3,8 +3,18 @@ import NewAnecdote from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notifications from "./components/Notification";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import anecedoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecedoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, []);
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,14 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
-
+import anecdoteService from "../services/anecdotes";
 const NewAnecdote = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
+    const anecdote = await anecdoteService.createNew(content);
     dispatch(createAnecdote(content));
   };
 
